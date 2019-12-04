11235e83b9d744ec76689a3b8178f4d4e853c410
Raudel Costa <raudel.costa@globant.com>
2019-12-04 11:17:22 -0300

    Fixing error de TakeUntil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from "angularx-social-login";
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
