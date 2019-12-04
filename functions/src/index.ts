8af01d486eba87ab1fab4cb25732fac1c864e061
Raudel Costa <raudel.costa@globant.com>
2019-12-04 14:47:57 -0300

    Agregando firebase al proyecto para integrarlos con un backend desarrollado en firebase

---
+++ functions/src/index.ts
@@ -0,0 +1,24 @@
+import * as functions from 'firebase-functions';
+const admin= require('firebase-admin');
+
+admin.initializeApp();
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+ export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+ });
+
+ export const getConfig = functions.https.onRequest((request, response) => {
+   admin.firestore().collection('navbar')
+   .get()
+   .then((data:any)=>{
+     let test:any=[];
+     data.forEach((doc : any) => {
+       test.push(doc.data())
+      });
+      return response.json(test);
+   })
+   .catch((error:any)=> console.log(error));
+ });
+
