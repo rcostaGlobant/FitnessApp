6ccc7de912a42718eac722f6a956f9e21207e60b
Raudel Costa <raudel.costa@globant.com>
2019-12-12 17:47:42 -0300

    Agregando modulo administrador

--- src/app/components/admin/dashboard-admin-home/dashboard-admin-home.component.ts
+++ src/app/components/admin/dashboard-admin-home/dashboard-admin-home.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from 'src/app/services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard-admin-home',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-admin-home.component.scss']
 })
 export class DashboardAdminHomeComponent implements OnInit {
+  private unsubscribe$= new Subject<void>();
+  userImg: any = null;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser(){
+    this.authService.isUserLogIn()
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe(userAuth => {
+      if(!!userAuth ) {
+
+        this.userImg = userAuth.photoURL;
+      } else {
+        this.userImg = null;
+      }
+    });
+  }
+
+  signOut(): void {
+    this.authService.logOut();
   }
 
 }
--- src/app/modules/feature/admin/admin.module.ts
+++ src/app/modules/feature/admin/admin.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardAdminHomeComponent } from '../../../components/admin/dashboard-admin-home/dashboard-admin-home.component';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [DashboardAdminHomeComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    SharedModule,
     AdminRoutingModule
   ]
 })
